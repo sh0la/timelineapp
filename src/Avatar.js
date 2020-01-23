8054f35c77d64c798995a3c5a1d7269ff24bbc9d
sh0la <sholaadeniyi@gmail.com>
2020-01-23 06:24:11 -0600

    Add user avatar component

---
+++ src/Avatar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Avatar() {
+  return (
+    <div className='avatar'>
+      <img 
+          src="./img/bluesedan.jpg" 
+          alt=""
+      />
+    </div>
+  )
+}
+
+export default Avatar;
