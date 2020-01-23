cd11c101687d1d8501ec9649dca24abc458ef2de
sh0la <sholaadeniyi@gmail.com>
2020-01-23 06:31:33 -0600

    Add data from parent HistoryTimeline

--- src/HistoryTimeline.js
+++ src/HistoryTimeline.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Post from './Post'
 
 function HistoryTimeline() {
 
   return (
     <main>
-      <Post />
+      <Post time='Just Now'/>
+      <Post time='Today @ 10:14 AM'/>
+      <Post time='Today @ 6:00 AM'/>
+      <Post time='Yesterday @ 11:30PM'/>
+      <Post time='Sunday @ 12:40 PM'/>
+      <Post time='January 5 @ 9:15 AM'/>
     </main>
   )
 }
