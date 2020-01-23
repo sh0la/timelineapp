7ae4e6f37b5b689729b90ccb17343ffe4013da9b
sh0la <sholaadeniyi@gmail.com>
2020-01-23 05:34:47 -0600

    Create the primary component

---
+++ src/TimelineApp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+
+
+class TimelineApp extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
+
+export default TimelineApp;
