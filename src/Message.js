f44151bac1301c3140520fb3b339cc9a826378fb
sh0la <sholaadeniyi@gmail.com>
2020-01-23 06:20:06 -0600

    Add functionality to view user posting history

---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Message() {
+  return (
+    <div className='message'>
+      Repellendus sed obcaecati. Possimus nisi animi dolorem omnis mollitia.
+    </div>
+  )
+}
+
+export default Message;
